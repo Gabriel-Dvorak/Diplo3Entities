f1f531cea5b2b169ae277c1a260339583c7611d5
Gabriel Dvorak <dvo230760@spengergasse.at>
2025-10-11 22:51:51 +0200

    Pos

--- src/main/java/at/spengergasse/diplo3entities/service/TextSectionService.java
+++ src/main/java/at/spengergasse/diplo3entities/service/TextSectionService.java
@@ -11,25 +11,4 @@ import java.util.List;
 @Service
 public class TextSectionService {
 
-    private final TextSectionRepository repository;
-
-    @Autowired
-    public TextSectionService(TextSectionRepository repository) {
-        this.repository = repository;
-    }
-
-    public void addTextSection(TextSection section) {
-        if (section.isTitle()) {
-            boolean exists = repository.existsByFrontendAreaAndIsTitleTrue(section.getFrontendArea());
-            if (exists) {
-                throw new IllegalStateException("Bereich " + section.getFrontendArea() + " hat bereits einen Titel!");
-            }
-        }
-        repository.save(section);
-    }
-
-    public interface TextSectionRepository extends JpaRepository<TextSection, Long> {
-        boolean existsByFrontendAreaAndIsTitleTrue(FrontendArea area);
-        List<TextSection> findAllByFrontendAreaOrderByPositionAsc(FrontendArea area);
-    }
 }
